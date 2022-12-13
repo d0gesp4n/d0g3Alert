496be0aa304e4c4866f434f9dfa633afd41d4cc3
w3legue@dev1 <w3legue@gmail.com>
2022-12-13 21:35:22 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,104 @@
 # d0g3Alert
+
+## Introduction
+
+This Elastalert alerter was created with Security Onion in mind, but it can be utilized for any elastalert deployment. The purpose of this alerter is to create a doc in a new or existing index that you can easily refer back to. Elastalert does this by default, but that log is more for logging purposes as opposed to highlighting interesting activity within the environment. This way you can have a separate index for alerts. 
+
+![Hunt Screenshot](huntExample.png)
+
+## Get Started
+
+To utilize this module as-is, copy the repo and add `d0g3Alert-es.py` to the `elastalert/modules/custom/` directory. After placing it there, restart the Elastalert service. You will also need to restart the service any time you modify an alerter.
+
+Once you have the alerter in-place, you can utilize the `d0g3alert-template.yaml` template file to create your first alert.
+
+If you would like to have it generate a link to pivot to, be sure to change the hostname in lines 130.
+```Python
+        payload.update({'hunt_link': 'https://<replace with secOnion host address>/#/hunt?q='+create_link(match, self.rule['link.filters'])})
+```          
+
+## Put it in Prod
+
+Start by ensuring the `elasticsearch_host` location is correct. The template requires 4 fields to work; `rule.title, rule.id, rule.category, rule.severity`. 
+
+*Example Required fields:*
+```Yaml
+# Elasticsearch Host
+elasticsearch_host: "localhost:9200"
+
+# (Required)
+# Rule name, must be unique
+name: Template D0g3Alert
+
+# required fields for D0g3Alert
+rule.title: "Template D0g3Alert"
+rule.id: "999999"
+rule.severity: 3
+rule.category: "ssh"
+```
+
+Once the required fields have been setup, we can move onto the optional fields; `event.fields, event.module, event.dataset, mitre.id, mitre.name, link.filters`
+
+*Example Optional fields:*
+```Yaml
+# optional fields
+# Default is alert
+event.dataset: "alert"
+# Default is d0g3Alert
+event.module: "d0g3Alert" 
+# fields to add to EA generated doc, must be list
+event.fields: 
+  - '@timestamp'
+  - destination.ip
+  - destination.port
+  - source.ip
+  - network.community_id
+# Mitre Fields
+# ID for the alert
+mitre.id: T1021
+mitre.name: Remote Services
+# Hunt link filter fields. must be list
+link.filters:
+  - network.community_id
+  - log.id.uid
+```
+
+**Field Descriptions**
+- *Required Fields*
+  - `rule.title`
+    : The name of the rule.
+  - `rule.id`
+    : ID for the rule. Can be used for rule tracking and tuning.
+  - `rule.category`
+    : Specify a category for the rule.
+  - `rule.severity`
+    : Specify the Severity as an integer of 1-4. It will add an additional `severity_level` field. The severities will translate as follows; 1:low, 2:medium, 3:high, 4:critical.
+- *Optional Fields*
+  - `event.dataset`
+      : Defaults to `alert` if omitted from rule
+  - `event.module`
+      : Defaults to `d0g3alert` if omitted from rule
+  - `event.fields`
+      : Specify fields that you want to be pulled from the matching doc and written to the new alert doc. Defaults to all fields from matching doc if omitted from rule
+  - `mitre.id`
+      : Specify the Mitre technique ID associated with this rule. If omitted, the field will not populate. Maps to `rule.mitre.id`.
+  - `mitre.name`
+      : Specify the Mitre technique name associated with this rule. If omitted, the field will not populate. Maps to `rule.mitre.technique`.
+  - `link.filters`
+      : Creates a new field called `hunt_link` and generates a link based on the fields provided. If omitted, the field will not populate.
+
+**Elastalert Fields**
+As with any Elastalert rule, you will need to specify the type, index, and filters. More info about those can be found in the [Elastalert Docs](https://elastalert.readthedocs.io/en/latest/running_elastalert.html#creating-a-rule)
+
+Example Rule type and filter:
+```Yaml
+# alert type / filter
+type: any
+index: "*:so-zeek-*"
+
+filter:
+- query:
+    query_string:
+      query: "destination.port:22"
+```
+
---
+++ d0g3Alert-es.py
@@ -0,0 +1,146 @@
+# path for new modules /opt/so/conf/elastalert/modules/custom/
+
+# -*- coding: utf-8 -*-
+
+from time import gmtime, strftime
+import requests,json,urllib.parse
+from elastalert.alerts import Alerter
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+class D0g3Alert(Alerter):
+  """
+  Use matched data to create alerts in elasticsearch
+  """
+  required_options = set(['rule.title','rule.id','rule.category','rule.severity'])
+
+  def alert(self, matches):
+    for match in matches:
+      today = strftime("%Y.%m.%d", gmtime())
+      timestamp = strftime("%Y-%m-%d"'T'"%H:%M:%S"'.000Z', gmtime())
+      headers = {"Content-Type": "application/json"}
+
+      creds = None
+      if 'es_username' in self.rule and 'es_password' in self.rule:
+        creds = (self.rule['es_username'], self.rule['es_password'])
+      
+      # set rule_level
+      alert_severity = self.rule['rule.severity']
+      if alert_severity == 4:
+        rule_level = 'critical'
+      elif alert_severity == 3:
+        rule_level = 'high'
+      elif alert_severity == 2:
+        rule_level = 'medium'
+      else:
+        rule_level == 'low'
+      
+      # set event fields
+      if 'event.dataset' in self.rule.keys():
+        event_dataset = self.rule['event.dataset']
+      else:
+        event_dataset = 'alert'
+      if 'event.module' in self.rule.keys():
+        event_module = self.rule['event.module']
+      else:
+        event_module = 'd0g3alert'
+      
+      payload = {
+        "rule": {
+          "name": self.rule['rule.title'],
+          "uuid": self.rule['rule.id'],
+          "category": self.rule['rule.category'] 
+          },
+        "event":{
+          "dataset": event_dataset,
+          "module": event_module,
+          "severity": self.rule['rule.severity'],
+          "severity_label": rule_level,
+          "timestamp": timestamp 
+          }
+      }
+      
+      # grab mitre fields
+      def setMitreFields():
+        mitre_dict = {'rule':{'mitre':{}}}
+        if 'mitre.id' and 'mitre.name' in self.rule.keys():
+          mitre_dict['rule']['mitre'].update({'id': self.rule['mitre.id']})          
+          mitre_dict['rule']['mitre'].update({'technique': self.rule['mitre.name']})          
+          return mitre_dict
+        else:
+          return
+      
+      def createDictionary(obs, searchFields):
+        output_dict = {}
+        for field in searchFields:
+          if '.' in field:
+            field = field.split('.')
+          else:
+            try:
+              output_dict[field] = obs[field]
+            except Exception as e:
+              exception = f"{type(e).__name__}: {e}"
+              print(f"{exception}")
+            continue
+
+          if field[0] in output_dict.keys():
+            try:
+              output_dict[field[0]].update({field[1]: obs[field[0]][field[1]]})
+            except Exception as e:
+              exception = f"{type(e).__name__}: {e}"
+              print(f"{exception}")
+          else:
+            try:
+              output_dict[field[0]] = {field[1]: obs[field[0]][field[1]]}
+            except Exception as e:
+              exception = f"{type(e).__name__}: {e}"
+              print(f"{exception}")
+
+        return output_dict
+      
+      def create_link(obs, filters):
+        linkList = []
+        linkString = ''
+        for field in filters:
+          try:
+            if '.' in field:
+              field = field.split('.')
+              temp = obs
+              for num in range(len(field)):
+                temp = temp[field[num]] # todo add check for non-exist field
+              linkList.append(temp)
+            else:
+              linkList.append(obs[field])          
+          except Exception as e:
+            exception = f"{type(e).__name__}: {e}"
+            print(f"{exception}")
+
+        for item in linkList:
+          if len(linkString) == 0:
+            linkString = f'"{item}"'
+          else:
+            linkString = linkString + f' OR "{item}"'
+        linkString = '('+linkString+')'+' | groupby "event.module" "event.dataset"'
+        linkString = urllib.parse.quote(linkString)
+        return linkString
+      
+      # set additional payload fields
+      if 'link.filters' in self.rule.keys():
+        payload.update({'hunt_link': 'https://<replace with secOnion host address>/#/hunt?q='+create_link(
+          match, self.rule['link.filters'])})
+      
+      if 'event.fields' in self.rule.keys():
+        payload.update(createDictionary(match, self.rule['event.fields']))
+      else:
+        payload.update(match)
+      
+      if 'mitre.id' and 'mitre.name' in self.rule.keys():
+        payload.update(setMitreFields())
+      # set ES url
+      url = f"https://{self.rule['es_host']}:{self.rule['es_port']}/so-d0g3alert-alerts-{today}/_doc/"
+      # post to ES index
+      requests.post(url, data=json.dumps(payload), headers=headers, verify=False, auth=creds)
+
+  def get_info(self):
+    return {'type': 'D0g3Alert'}
